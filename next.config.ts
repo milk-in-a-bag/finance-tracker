f26bff0ba250712c2ec55e337237df1fe9c3ec2b
milk-in-a-bag <bryanmayodi@gmail.com>
2026-09-18 13:35:45 +0300

    fix prisma-on-vercel issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./src/generated/prisma/**/*"],
+    "/dashboard": ["./src/generated/prisma/**/*"],
+  },
 };
 
 export default nextConfig;
